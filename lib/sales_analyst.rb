6b96e0576efc58eb9600c04beef5469e2ef11ee3
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-05-27 21:24:59 -0600

    Standard deviations, merchants_with_high_item_count both tested and working. Imported merchant list into sales_analyst.

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,17 +1,46 @@
 require_relative 'sales_engine'
 
 class SalesAnalyst
-  attr_reader :parent, :items, :items_by_merchant
+  attr_reader :parent, :items, :items_by_merchant, :merchants, :high_item_count_list
 
   def initialize(parent)
     @parent = parent
     @items ||= get_items
+    @merchants ||= get_merchants
     @items_by_merchant ||= group_items
+    @high_item_count_list ||= list_of_high_item_count_merchant_ids
+  end
+
+  def average_items_per_merchant
+    BigDecimal.new((@items.length.to_f / @items_by_merchant.length), 3).to_f
+  end
+
+  def average_items_per_merchant_standard_deviation
+    BigDecimal.new(Math.sqrt(sum_of_deviations / (@items_by_merchant.length - 1)), 3).to_f
+  end
+
+  def merchants_with_high_item_count
+    @merchants.find_all do |merchant|
+      @high_item_count_list.include?(merchant.id)
+    end
+  end
+
+  def list_of_high_item_count_merchant_ids
+    @items_by_merchant.map do |merchant_id, merchant|
+      if merchant.count > (average_items_per_merchant_standard_deviation * 2)
+        merchant_id
+      end
+    end.compact.uniq
   end
 
   def get_items
     item_repo = parent.from_csv({:items => "./data/items.csv"})
     item_repo.items.items
+  end
+
+  def get_merchants
+    merchant_repo = parent.from_csv({:merchants => "./data/merchants.csv"})
+    merchant_repo.merchants.merchants
   end
 
   def group_items
@@ -23,4 +52,24 @@ class SalesAnalyst
       end
     end
   end
+
+  def list_of_deviations
+    mean = average_items_per_merchant
+    @items_by_merchant.map do |merchant_id, merchant|
+      merchant.count - mean
+    end
+  end
+
+  def square_deviations
+    list_of_deviations.map do |deviation|
+      deviation ** 2
+    end
+  end
+
+  def sum_of_deviations
+    square_deviations.inject do |sum, deviation|
+      sum + deviation
+    end
+  end
+
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -12,12 +12,64 @@ class SalesAnalystTest < Minitest::Test
 
   def test_it_gets_items
     sa = SalesEngine.analyst
-    binding.pry
     assert_equal 1367, sa.items.length
+  end
+
+  def test_it_gets_merchants
+    sa = SalesEngine.analyst
+    assert_equal 475, sa.merchants.length
   end
 
   def test_it_groups_items_by_merchant
     sa = SalesEngine.analyst
     assert_equal 475, sa.items_by_merchant.length
+  end
+
+  def test_average_items_per_merchant
+    sa = SalesEngine.analyst
+    assert_equal 2.88, sa.average_items_per_merchant
+    assert_equal Float, sa.average_items_per_merchant.class
+  end
+
+  def test_list_of_deviations
+    sa = SalesEngine.analyst
+    deviations = sa.list_of_deviations
+    assert_equal Array, deviations.class
+    assert_equal 475, deviations.length
+    assert_equal -1.88, deviations[0]
+  end
+
+  def test_square_deviations
+    sa = SalesEngine.analyst
+    square_deviations = sa.square_deviations
+    assert_equal Array, square_deviations.class
+    assert_equal 475, square_deviations.length
+    assert_equal 3.5343999999999998,  square_deviations[0]
+  end
+
+  def test_sum_of_deviations
+    sa = SalesEngine.analyst
+    sum = sa.sum_of_deviations
+    assert_equal Float, sum.class
+    assert_equal 5034.919999999936, sum
+  end
+
+  def test_average_items_per_merchant_standard_deviation
+    sa = SalesEngine.analyst
+    standard_deviation = sa.average_items_per_merchant_standard_deviation
+    assert_equal 3.26, standard_deviation
+    assert_equal Float, standard_deviation.class
+  end
+
+  def test_get_list_of_high_item_count_merchant_ids
+    sa = SalesEngine.analyst
+    assert_equal Array, sa.high_item_count_list.class
+    assert_equal 52, sa.high_item_count_list.length
+  end
+
+  def test_merchants_with_high_item_count
+    sa = SalesEngine.analyst
+    assert_equal 52, sa.merchants_with_high_item_count.length
+    assert_equal Merchant, sa.merchants.first.class
   end
 end
