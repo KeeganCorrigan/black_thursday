faea460b52c689306525e9170c4ace3c3fc05ad2
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-05-29 19:03:37 -0600

    Fixed error with cc expiration date.

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -10,7 +10,13 @@ class CsvParser
   end
 
   def load_csv(csv_file)
-    data = CSV.foreach(csv_file, headers: true, header_converters: :symbol, converters: :all).map { |row| row.to_h }
+    if csv_file == "./data/transactions.csv"
+      data = CSV.foreach(csv_file, headers: true, header_converters: :symbol).map { |row|
+          row.to_h }
+    else
+      data = CSV.foreach(csv_file, headers: true, header_converters: :symbol, converters: :all).map { |row|
+          row.to_h }
+    end
     return data
   end
 end
